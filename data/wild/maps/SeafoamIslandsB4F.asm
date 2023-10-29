7356f7a60781c5138bb761d5bb1b47a710185b3b
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:56:31 +0000

    Update SeafoamIslandsB4F.asm

--- data/wild/maps/SeafoamIslandsB4F.asm
+++ data/wild/maps/SeafoamIslandsB4F.asm
@@ -1,15 +1,15 @@
 SeafoamIslandsB4FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 31, HORSEA
-	db 31, SHELLDER
-	db 33, HORSEA
-	db 33, SHELLDER
-	db 29, SLOWPOKE
-	db 31, SEEL
-	db 31, SLOWPOKE
-	db 29, SEEL
-	db 39, SLOWBRO
+	db 48, HORSEA
+	db 48, SHELLDER
+	db 51, HORSEA
+	db 51, SHELLDER
+	db 45, SLOWPOKE
+	db 48, SEEL
+	db 48, SLOWPOKE
+	db 45, SEEL
+	db 60, SLOWBRO
 ENDC
 IF DEF(_BLUE)
 	db 31, KRABBY
@@ -22,7 +22,7 @@ IF DEF(_BLUE)
 	db 29, SEEL
 	db 39, GOLDUCK
 ENDC
-	db 32, GOLBAT
+	db 49, GOLBAT
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
