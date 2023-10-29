e5deea0bc99e7719f855b5bcf3ccc14a376026bb
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:11:26 +0000

    Update RockTunnel1F.asm

--- data/wild/maps/RockTunnel1F.asm
+++ data/wild/maps/RockTunnel1F.asm
@@ -1,15 +1,15 @@
 RockTunnel1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, ZUBAT
-	db 17, ZUBAT
-	db 17, GEODUDE
-	db 15, MACHOP
-	db 16, GEODUDE
-	db 18, ZUBAT
-	db 15, ZUBAT
-	db 17, MACHOP
-	db 13, ONIX
-	db 15, ONIX
+	db 20, ZUBAT
+	db 21, ZUBAT
+	db 21, GEODUDE
+	db 19, MACHOP
+	db 20, GEODUDE
+	db 22, ZUBAT
+	db 19, ZUBAT
+	db 31, MACHOP
+	db 16, ONIX
+	db 19, ONIX
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
