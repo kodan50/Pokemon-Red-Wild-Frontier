5faf3980eeb358a787efc37fc544a160eb516eca
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:25:31 +0000

    Update Route16.asm

--- data/wild/maps/Route16.asm
+++ data/wild/maps/Route16.asm
@@ -1,15 +1,15 @@
 Route16WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
-	db 18, RATTATA
-	db 20, DODUO
-	db 20, RATTATA
-	db 18, DODUO
-	db 22, DODUO
-	db 22, RATTATA
-	db 23, RATICATE
-	db 25, RATICATE
+	db 28, SPEAROW
+	db 31, SPEAROW
+	db 26, RATTATA
+	db 28, DODUO
+	db 28, RATTATA
+	db 26, DODUO
+	db 31, DODUO
+	db 31, RATTATA
+	db 33, RATICATE
+	db 35, RATICATE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
