026ded8ac74740d7e3153e2327efa8e825d641a2
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:26:57 +0000

    Update Route18.asm

--- data/wild/maps/Route18.asm
+++ data/wild/maps/Route18.asm
@@ -1,15 +1,15 @@
 Route18WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, SPEAROW
-	db 22, SPEAROW
-	db 25, RATICATE
-	db 24, DODUO
-	db 25, FEAROW
-	db 26, DODUO
-	db 28, DODUO
-	db 29, RATICATE
-	db 27, FEAROW
-	db 29, FEAROW
+	db 29, SPEAROW
+	db 32, SPEAROW
+	db 36, RATICATE
+	db 35, DODUO
+	db 36, FEAROW
+	db 38, DODUO
+	db 41, DODUO
+	db 42, RATICATE
+	db 39, FEAROW
+	db 42, FEAROW
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
