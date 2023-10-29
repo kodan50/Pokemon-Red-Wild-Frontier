999ab7073fd78a0521c7f8907ba8e52f3bfe5d60
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:45:17 +0000

    Update Route8.asm

--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -1,16 +1,16 @@
 Route8WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 18, PIDGEY
+	db 23, PIDGEY
 IF DEF(_RED)
-	db 18, MANKEY
-	db 17, EKANS
-	db 16, GROWLITHE
-	db 20, PIDGEY
-	db 20, MANKEY
-	db 19, EKANS
-	db 17, GROWLITHE
-	db 15, GROWLITHE
-	db 18, GROWLITHE
+	db 23, MANKEY
+	db 22, EKANS
+	db 20, GROWLITHE
+	db 25, PIDGEY
+	db 25, MANKEY
+	db 24, EKANS
+	db 22, GROWLITHE
+	db 19, GROWLITHE
+	db 23, GROWLITHE
 ENDC
 IF DEF(_BLUE)
 	db 18, MEOWTH
