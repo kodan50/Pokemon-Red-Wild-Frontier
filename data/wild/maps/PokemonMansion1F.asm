e5e333c2219dc0a1faee01ec4bc974c41e50c774
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 22:45:13 +0000

    Update PokemonMansion1F.asm

--- data/wild/maps/PokemonMansion1F.asm
+++ data/wild/maps/PokemonMansion1F.asm
@@ -1,16 +1,16 @@
 PokemonMansion1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 32, KOFFING
-	db 30, KOFFING
-	db 34, PONYTA
-	db 30, PONYTA
-	db 34, GROWLITHE
-	db 32, PONYTA
-	db 30, GRIMER
-	db 28, PONYTA
-	db 37, WEEZING
-	db 39, MUK
+	db 50, KOFFING
+	db 47, KOFFING
+	db 53, PONYTA
+	db 47, VULPIX
+	db 53, GROWLITHE
+	db 50, VULPIX
+	db 47, GRIMER
+	db 44, PONYTA
+	db 57, WEEZING
+	db 61, MUK
 ENDC
 IF DEF(_BLUE)
 	db 32, GRIMER
