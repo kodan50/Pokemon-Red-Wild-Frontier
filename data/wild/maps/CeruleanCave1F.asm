2f8946c57397a15fa6a2c0806fb19688fee73c29
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 22:30:09 +0000

    Update CeruleanCave1F.asm

--- data/wild/maps/CeruleanCave1F.asm
+++ data/wild/maps/CeruleanCave1F.asm
@@ -1,20 +1,20 @@
 CeruleanCave1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 46, GOLBAT
-	db 46, HYPNO
-	db 46, MAGNETON
-	db 49, DODRIO
-	db 49, VENOMOTH
+	db 75, GOLBAT
+	db 75, HYPNO
+	db 75, MAGNETON
+	db 80, DODRIO
+	db 80, VENOMOTH
 IF DEF(_RED)
-	db 52, ARBOK
+	db 85, ARBOK
 ENDC
 IF DEF(_BLUE)
 	db 52, SANDSLASH
 ENDC
-	db 49, KADABRA
-	db 52, PARASECT
-	db 53, RAICHU
-	db 53, DITTO
+	db 80, KADABRA
+	db 85, PARASECT
+	db 85, RAICHU
+	db 85, DITTO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
