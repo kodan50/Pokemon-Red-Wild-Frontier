cc3af6b797f5583fac25ee92b9bf65de51c40795
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:37:08 +0000

    Update Route25.asm

--- data/wild/maps/Route25.asm
+++ data/wild/maps/Route25.asm
@@ -1,16 +1,16 @@
 Route25WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db  8, WEEDLE
-	db  9, KAKUNA
-	db 13, PIDGEY
-	db 12, ODDISH
+	db  9, WEEDLE
+	db 10, KAKUNA
+	db 15, PIDGEY
 	db 13, ODDISH
-	db 12, ABRA
-	db 14, ODDISH
-	db 10, ABRA
-	db  7, METAPOD
-	db  8, CATERPIE
+	db 15, ODDISH
+	db 13, ABRA
+	db 16, ODDISH
+	db 11, ABRA
+	db  8, METAPOD
+	db  9, CATERPIE
 ENDC
 IF DEF(_BLUE)
 	db  8, CATERPIE
