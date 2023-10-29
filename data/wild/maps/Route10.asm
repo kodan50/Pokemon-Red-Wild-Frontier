bb8e56fceda488355a84c5c3ff80139f713a88fe
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:19:00 +0000

    Update Route10.asm

--- data/wild/maps/Route10.asm
+++ data/wild/maps/Route10.asm
@@ -1,16 +1,16 @@
 Route10WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, VOLTORB
-	db 16, SPEAROW
-	db 14, VOLTORB
-IF DEF(_RED)
-	db 11, EKANS
-	db 13, SPEAROW
-	db 15, EKANS
+	db 20, VOLTORB
+	db 20, SPEAROW
 	db 17, VOLTORB
-	db 17, SPEAROW
-	db 13, EKANS
-	db 17, EKANS
+IF DEF(_RED)
+	db 14, EKANS
+	db 16, SPEAROW
+	db 18, EKANS
+	db 21, VOLTORB
+	db 21, SPEAROW
+	db 16, EKANS
+	db 21, EKANS
 ENDC
 IF DEF(_BLUE)
 	db 11, SANDSHREW
