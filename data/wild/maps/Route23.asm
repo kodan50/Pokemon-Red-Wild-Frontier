728403049cfa894c453c75dca5544239ceb3720e
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:35:23 +0000

    Update Route23.asm

--- data/wild/maps/Route23.asm
+++ data/wild/maps/Route23.asm
@@ -1,25 +1,25 @@
 Route23WildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 26, EKANS
+	db 42, EKANS
 ENDC
 IF DEF(_BLUE)
 	db 26, SANDSHREW
 ENDC
-	db 33, DITTO
-	db 26, SPEAROW
-	db 38, FEAROW
-	db 38, DITTO
-	db 38, FEAROW
+	db 53, DITTO
+	db 42, SPEAROW
+	db 61, FEAROW
+	db 61, DITTO
+	db 61, FEAROW
 IF DEF(_RED)
-	db 41, ARBOK
+	db 65, ARBOK
 ENDC
 IF DEF(_BLUE)
 	db 41, SANDSLASH
 ENDC
-	db 43, DITTO
-	db 41, FEAROW
-	db 43, FEAROW
+	db 68, DITTO
+	db 65, SANDSHREW
+	db 68, FEAROW
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
