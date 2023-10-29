38f661def9e51238883762fca9439a030ab53c3f
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:45:56 +0000

    Update Route9.asm

--- data/wild/maps/Route9.asm
+++ data/wild/maps/Route9.asm
@@ -1,16 +1,16 @@
 Route9WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 16, RATTATA
-	db 16, SPEAROW
-	db 14, RATTATA
-IF DEF(_RED)
-	db 11, EKANS
-	db 13, SPEAROW
-	db 15, EKANS
+	db 19, RATTATA
+	db 19, SPEAROW
 	db 17, RATTATA
-	db 17, SPEAROW
+IF DEF(_RED)
 	db 13, EKANS
-	db 17, EKANS
+	db 16, SPEAROW
+	db 18, EKANS
+	db 21, RATTATA
+	db 21, SPEAROW
+	db 16, EKANS
+	db 21, EKANS
 ENDC
 IF DEF(_BLUE)
 	db 11, SANDSHREW
