f01166dd6948781d7bb6034656dcf3cf536d7c60
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 22:30:59 +0000

    Update CeruleanCave2F.asm

--- data/wild/maps/CeruleanCave2F.asm
+++ data/wild/maps/CeruleanCave2F.asm
@@ -1,15 +1,15 @@
 CeruleanCave2FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 51, DODRIO
-	db 51, VENOMOTH
-	db 51, KADABRA
-	db 52, RHYDON
-	db 52, MAROWAK
-	db 52, ELECTRODE
-	db 56, CHANSEY
-	db 54, WIGGLYTUFF
-	db 55, DITTO
-	db 60, DITTO
+	db 83, DODRIO
+	db 83, VENOMOTH
+	db 83, KADABRA
+	db 85, RHYDON
+	db 85, MAROWAK
+	db 85, ELECTRODE
+	db 91, CHANSEY
+	db 88, WIGGLYTUFF
+	db 90, DITTO
+	db 98, DITTO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
