a5f72a91d77f238e17f41a7cd09ec90b0b8f27db
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:22:08 +0000

    Update Route12.asm

--- data/wild/maps/Route12.asm
+++ data/wild/maps/Route12.asm
@@ -1,16 +1,16 @@
 Route12WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db 24, ODDISH
-	db 25, PIDGEY
-	db 23, PIDGEY
-	db 24, VENONAT
-	db 22, ODDISH
-	db 26, VENONAT
-	db 26, ODDISH
-	db 27, PIDGEY
-	db 28, GLOOM
-	db 30, GLOOM
+	db 32, ODDISH
+	db 33, PIDGEY
+	db 30, BELLSPROUT
+	db 32, VENONAT
+	db 29, ODDISH
+	db 34, VENONAT
+	db 34, ODDISH
+	db 36, PIDGEY
+	db 37, GLOOM
+	db 39, GLOOM
 ENDC
 IF DEF(_BLUE)
 	db 24, BELLSPROUT
