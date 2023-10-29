68a1dce84021212b059b2d6570aa125878d362da
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:54:19 +0000

    Update SeafoamIslandsB1F.asm

--- data/wild/maps/SeafoamIslandsB1F.asm
+++ data/wild/maps/SeafoamIslandsB1F.asm
@@ -1,16 +1,16 @@
 SeafoamIslandsB1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 30, STARYU
-	db 30, HORSEA
-	db 32, SHELLDER
-	db 32, HORSEA
-	db 28, SLOWPOKE
-	db 30, SEEL
-	db 30, SLOWPOKE
-	db 28, SEEL
-	db 38, DEWGONG
-	db 37, SEADRA
+	db 46, STARYU
+	db 46, HORSEA
+	db 49, SHELLDER
+	db 49, HORSEA
+	db 43, SLOWPOKE
+	db 46, SEEL
+	db 46, SLOWPOKE
+	db 43, SEEL
+	db 58, DEWGONG
+	db 57, SEADRA
 ENDC
 IF DEF(_BLUE)
 	db 30, SHELLDER
