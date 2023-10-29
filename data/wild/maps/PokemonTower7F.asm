030b1692c09e02dfb43884a7b02d3fbb21fea7b0
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:05:37 +0000

    Update PokemonTower7F.asm

--- data/wild/maps/PokemonTower7F.asm
+++ data/wild/maps/PokemonTower7F.asm
@@ -1,15 +1,15 @@
 PokemonTower7FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 21, GASTLY
-	db 22, GASTLY
-	db 23, GASTLY
-	db 24, GASTLY
-	db 20, GASTLY
-	db 28, HAUNTER
-	db 22, CUBONE
-	db 24, CUBONE
-	db 28, HAUNTER
-	db 30, HAUNTER
+	db 26, GASTLY
+	db 27, GASTLY
+	db 29, GASTLY
+	db 30, GASTLY
+	db 25, GASTLY
+	db 35, HAUNTER
+	db 27, CUBONE
+	db 30, CUBONE
+	db 35, HAUNTER
+	db 37, HAUNTER
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
