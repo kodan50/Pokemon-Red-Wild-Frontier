74b0fb214bc6b04953ae8736692f06394171d8d4
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-29 23:24:34 +0000

    Update Route15.asm

--- data/wild/maps/Route15.asm
+++ data/wild/maps/Route15.asm
@@ -1,14 +1,14 @@
 Route15WildMons:
 	def_grass_wildmons 15 ; encounter rate
 IF DEF(_RED)
-	db 24, ODDISH
-	db 26, DITTO
-	db 23, PIDGEY
-	db 26, VENONAT
-	db 22, ODDISH
-	db 28, VENONAT
-	db 26, ODDISH
-	db 30, GLOOM
+	db 33, ODDISH
+	db 36, DITTO
+	db 32, PIDGEY
+	db 36, VENONAT
+	db 30, ODDISH
+	db 36, VENONAT
+	db 36, ODDISH
+	db 41, GLOOM
 ENDC
 IF DEF(_BLUE)
 	db 24, BELLSPROUT
@@ -20,8 +20,8 @@ IF DEF(_BLUE)
 	db 26, BELLSPROUT
 	db 30, WEEPINBELL
 ENDC
-	db 28, PIDGEOTTO
-	db 30, PIDGEOTTO
+	db 39, PIDGEOTTO
+	db 41, PIDGEOTTO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
