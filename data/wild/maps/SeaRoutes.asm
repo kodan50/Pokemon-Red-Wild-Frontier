73f212bb2beee221258bf8b090e8dd1e1a25e586
Daniel <38507909+kodan50@users.noreply.github.com>
2023-10-30 00:10:53 +0000

    Update SeaRoutes.asm

--- data/wild/maps/SeaRoutes.asm
+++ data/wild/maps/SeaRoutes.asm
@@ -3,14 +3,14 @@ SeaRoutesWildMons:
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
+	db  5, TENTACOOL
 	db  8, TENTACOOL
-	db 16, TENTACOOL
-	db 24, TENTACOOL
-	db  8, GOLDEEN
-	db 16, GOLDEEN
-	db 24, GOLDEEN
-	db  8, MAGIKARP
-	db 16, MAGICARP
-	db 24, TENTACRUEL
-	db 63, TENTACRUEL
+	db 10, TENTACOOL
+	db 15, TENTACOOL
+	db 20, TENTACOOL
+	db 25, TENTACOOL
+	db 30, TENTACRUEL
+	db 35, TENTACRUEL
+	db 50, TENTACRUEL
+	db 65, TENTACRUEL
 	end_water_wildmons
